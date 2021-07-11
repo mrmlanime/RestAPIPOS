4a67869d0f12e47fabbd321f35e80722513532c5
mrmlanime <yanixmrml@gmail.com>
2021-07-11 23:50:37 +0800

    First Update

---
+++ src/test/java/org/yanixmrml/pos/POSTRestAppTests.java
@@ -0,0 +1,13 @@
+package org.yanixmrml.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class POSTRestAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
